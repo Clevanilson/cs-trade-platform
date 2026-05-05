5bd105fb76276fb9441f71081d0cf20febf6b952
Clevanilson <clevanilson.contato@gmail.com>
2026-05-04 22:56:28 -0300

    feat(di): implement dependency injection container

---
+++ packages/di/src/di.ts
@@ -0,0 +1,30 @@
+import type { InjectionToken } from "./injection-token";
+
+class DI {
+  private static instance: DI;
+  private readonly container = new Map<InjectionToken<unknown>, unknown>();
+
+  private constructor() {}
+
+  static getInstance(): DI {
+    if (!DI.instance) {
+      DI.instance = new DI();
+    }
+    return DI.instance;
+  }
+
+  register<TValue>(token: InjectionToken<TValue>, value: TValue): void {
+    this.container.set(token, value);
+  }
+
+  inject<TValue>(token: InjectionToken<TValue>): TValue {
+    const value = this.container.get(token);
+    if (value === undefined) {
+      throw new Error(`Dependency not found for token: ${token.name}`);
+    }
+    return value as TValue;
+  }
+}
+
+export const di = DI.getInstance();
+export { DI };
